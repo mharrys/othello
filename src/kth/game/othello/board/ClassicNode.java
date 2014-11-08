df3d58837abaea20f095b4388450af659eb0ae94
hygerth <hygerth@kth.se>
2014-11-08 15:48:52 +0100

    Can construct nodes

--- src/kth/game/othello/board/ClassicNode.java
+++ src/kth/game/othello/board/ClassicNode.java
@@ -4,30 +4,51 @@ package kth.game.othello.board;
  * Describes a node in a classic 8x8 Othello board.
  */
 public class ClassicNode implements Node {
-
+	
+	private String id;
+	private String occupantId;
+	private int x;
+	private int y;
+	private boolean marked;
+	
+	/**
+	 * Construct a node in a classic Othello board
+	 * 
+	 * @param id the id for the node
+	 * @param x the x-coordinate on the board
+	 * @param y the y-coordinate on the board
+	 */
+	public ClassicNode(String id, int x, int y) {
+		this.id = id;
+		this.occupantId = "";
+		this.x = x;
+		this.y = y;
+		this.marked = false;
+	}
+	
 	@Override
 	public String getId() {
-		return null;
+		return id;
 	}
 
 	@Override
 	public String getOccupantPlayerId() {
-		return null;
+		return occupantId;
 	}
 
 	@Override
 	public int getXCoordinate() {
-		return 0;
+		return x;
 	}
 
 	@Override
 	public int getYCoordinate() {
-		return 0;
+		return y;
 	}
 
 	@Override
 	public boolean isMarked() {
-		return false;
+		return marked;
 	}
 
 }
