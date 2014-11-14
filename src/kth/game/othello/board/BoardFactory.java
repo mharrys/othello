79364df7c005783fbcfce5a094a8e652e5e7e947
Mattias Harrysson <mathar@kth.se>
2014-11-14 12:43:03 +0100

    remove include and improve documentation

--- src/kth/game/othello/board/BoardFactory.java
+++ src/kth/game/othello/board/BoardFactory.java
@@ -2,7 +2,6 @@ package kth.game.othello.board;
 
 import java.util.List;
 
-import kth.game.othello.board.Board;
 import kth.game.othello.player.Player;
 
 /**
@@ -13,16 +12,20 @@ import kth.game.othello.player.Player;
 public interface BoardFactory {
 
 	/**
-	 * Creates an Board.
-	 * 
-	 * @return An Board game
+	 * Creates an board in a starting state from two specified players.
+	 *
+	 * @param player1 the first player
+	 * @param player2 the second player
+	 * @return constructed board game
 	 */
 	public Board constructBoard(int rows, int cols, Player player1, Player player2);
 
 	/**
-	 * Reconstructs a Board.
-	 * 
-	 * @return An Board game
+	 * Reconstructs a board with a collection of nodes to be occupied by specified player.
+	 *
+	 * @param nodesToSwap the nodes to be occupied by the player
+	 * @param playerId the player to occupy the nodes
+	 * @return constructed board game
 	 */
 	public Board constructBoard(List<Node> nodes, List<Node> nodesToSwap, String playerId);
 
