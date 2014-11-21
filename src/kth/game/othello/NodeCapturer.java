8b2237dff0a6bf81e68dff90749ad5614d4a27b4
hygerth <hygerth@kth.se>
2014-11-21 14:26:44 +0100

    Created NodeCapturer class

---
+++ src/kth/game/othello/NodeCapturer.java
@@ -0,0 +1,75 @@
+package kth.game.othello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kth.game.othello.board.Node;
+import kth.game.othello.player.Player;
+
+/**
+ * This class is responsible for calculating nodes to capture
+ * 
+ * @author Henrik Hygerth
+ *
+ */
+public class NodeCapturer {
+
+	private NodeFinder nodeFinder;
+	private int cols = 8;
+	private int rows = 8;
+
+	public NodeCapturer(NodeFinder nodeFinder) {
+		this.nodeFinder = nodeFinder;
+	}
+
+	/**
+	 * Returns the captured nodes that a player can make from the specified empty node in the specified direction which
+	 * is occupied by the opponent player.
+	 *
+	 * @param nodes list of all the nodes
+	 * @param player the moving player
+	 * @param from the empty node
+	 * @param direction the adjacent node occupied by the moving player opponent
+	 * @return list of nodes to be captured in this direction by the empty node
+	 */
+	public List<Node> nodesToCaptureInDirection(List<Node> nodes, Player player, Node from, Node direction) {
+		List<Node> captures = new ArrayList<Node>();
+
+		int x = from.getXCoordinate();
+		int y = from.getYCoordinate();
+		int adjX = direction.getXCoordinate();
+		int adjY = direction.getYCoordinate();
+
+		int stepX = adjX - x;
+		int stepY = adjY - y;
+		// start looking on the next adjacent node
+		x += stepX;
+		y += stepY;
+
+		boolean validCapture = false;
+		while (x >= 0 && y >= 0 && x < cols && y < rows) {
+			Node n = nodeFinder.getNodeFromGrid(nodes, x, y);
+
+			if (!n.isMarked()) {
+				// we hit a unmarked node before finding a node which was occupied by one of the moving players
+				break;
+			} else if (n.getOccupantPlayerId().equals(direction.getOccupantPlayerId())) {
+				captures.add(n);
+			} else if (n.getOccupantPlayerId().equals(player.getId())) {
+				validCapture = true;
+				break;
+			}
+
+			x += stepX;
+			y += stepY;
+		}
+
+		if (validCapture) {
+			return captures;
+		} else {
+			// could not embrace one or more opponent nodes
+			captures.clear();
+			return captures;
+		}
+	}
+}
