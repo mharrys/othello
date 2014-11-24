1743f3216676db677163880493208aee163eb919
hygerth <hygerth@kth.se>
2014-11-24 18:17:00 +0100

    Implemented new API methods for player

--- src/kth/game/othello/player/OthelloPlayer.java
+++ src/kth/game/othello/player/OthelloPlayer.java
@@ -1,15 +1,19 @@
 package kth.game.othello.player;
 
+import kth.game.othello.player.movestrategy.MoveStrategy;
+
 /**
  * Describes a Othello player.
  *
  * @author Mattias Harrysson
+ * @author Henrik Hygerth
  */
 public class OthelloPlayer implements Player {
 
 	private String id;
 	private String name;
 	private Type type;
+	private MoveStrategy moveStrategy;
 
 	/**
 	 * Constructs a Othello player with specified id, name and type.
@@ -18,10 +22,11 @@ public class OthelloPlayer implements Player {
 	 * @param name the player name
 	 * @param type the type of player
 	 */
-	public OthelloPlayer(String id, String name, Type type) {
+	public OthelloPlayer(String id, String name, Type type, MoveStrategy moveStrategy) {
 		this.id = id;
 		this.name = name;
 		this.type = type;
+		this.moveStrategy = moveStrategy;
 	}
 
 	@Override
@@ -37,6 +42,16 @@ public class OthelloPlayer implements Player {
 	@Override
 	public Type getType() {
 		return type;
+	}
+
+	@Override
+	public MoveStrategy getMoveStrategy() {
+		return moveStrategy;
+	}
+
+	@Override
+	public void setMoveStrategy(MoveStrategy moveStrategy) {
+		this.moveStrategy = moveStrategy;
 	}
 
 }
