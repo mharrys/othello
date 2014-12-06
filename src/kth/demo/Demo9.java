fb22a71e8e3d2dddc90adf54df59aac7c76e6fe7
hygerth <hygerth@kth.se>
2014-12-06 18:30:22 +0100

    Added demo for tournament module

---
+++ src/kth/demo/Demo9.java
@@ -0,0 +1,38 @@
+package kth.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kth.game.OthelloTournament;
+import kth.game.othello.player.Player;
+import kth.game.othello.player.PlayerImpl;
+import kth.game.othello.player.Player.Type;
+import kth.game.othello.player.movestrategy.AggressiveMoveStrategy;
+import kth.game.othello.player.movestrategy.NaiveMoveStrategy;
+import kth.game.othello.player.movestrategy.RandomMoveStrategy;
+
+/**
+ * Starts an Othello game tournament between four different computers on a classic board using either a GUI or console.
+ *
+ * @author Henrik Hygerth
+ */
+public class Demo9 {
+
+	/**
+	 * Demo entry point.
+	 */
+	public static void main(String[] args) {
+		List<Player> players = new ArrayList<Player>();
+		Player naivePlayer = new PlayerImpl("naivePlayer", "Naive Player", Type.COMPUTER, new NaiveMoveStrategy());
+		Player randomPlayer = new PlayerImpl("randomPlayer", "Random Player", Type.COMPUTER, new RandomMoveStrategy());
+		Player aggressivePlayer = new PlayerImpl("aggressivePlayer", "Aggressive Player", Type.COMPUTER, new AggressiveMoveStrategy());
+		
+		players.add(naivePlayer);
+		players.add(randomPlayer);
+		players.add(aggressivePlayer);
+		
+		OthelloTournament t = new OthelloTournament(players);
+		t.run();
+	}
+
+}
